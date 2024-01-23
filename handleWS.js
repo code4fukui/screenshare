11ca752adf697cbe7cdbff3ab9aaf6cb02b70658
Taisuke Fukuno <fukuno@jig.jp>
2024-01-24 05:51:22 +0900

    make nekocam

--- handleWS.js
+++ handleWS.js
@@ -1,11 +1,59 @@
-export const handleWS = (socket) => {
+import { createWebSocketCBOR } from "./static/createWebSocket.js";
+
+let cam = null;
+let running = false;
+const clients = [];
+
+export const handleWS = (socket0) => {
+  const socket = createWebSocketCBOR(socket0);
   socket.onopen = () => {
     console.log("CONNECTED");
   };
   socket.onmessage = (event) => {
-    console.log(`RECEIVED: ${event.data}`);
-    socket.send("pong");
+    const data = event.data;
+    const t = data.type;
+    //console.log(data, t);
+    console.log(t);
+    if (t == "init_cam") {
+      if (cam) {
+        try {
+          cam.send({ type: "end_cam" });
+        } catch (e) {
+        }
+      }
+      cam = socket;
+    } else if (t == "init_client") {
+      if (cam && !running) {
+        cam.send({ type: "start_cam" });
+        console.log("start cam");
+        running = true;
+      }
+      clients.push(socket);
+    } else if (t == "cam_image") {
+      console.log("img transfer", clients.length);
+      clients.forEach(i => {
+        try {
+          i.send({ type: "cam_image", img: data.img });
+        } catch (e) {
+        }
+      });
+    }
   };
-  socket.onclose = () => console.log("DISCONNECTED");
+  socket.onclose = () => {
+    for (let i = 0; i < clients.length; i++) {
+      if (clients[i] == socket) {
+        clients.splice(i, 1);
+        break;
+      }
+    }
+    if (clients.length == 0) {
+      if (running) {
+        running = false;
+        cam.send({ type: "stop_cam" });
+        console.log("stop cam");
+      }
+    }
+    console.log("DISCONNECTED");
+  };
   socket.onerror = (error) => console.error("ERROR:", error);
 };

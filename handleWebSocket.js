91cb82d9632f1281cfd5c9141a4e5ea66f273fb5
Taisuke Fukuno <fukuno@jig.jp>
2024-01-24 06:30:45 +0900

    support group

--- handleWebSocket.js
+++ handleWebSocket.js
@@ -1,6 +1,21 @@
-let cam = null;
-let running = false;
-const clients = [];
+class Group {
+  name;
+  cam = null;
+  running = false;
+  clients = [];
+  constructor(name) {
+    this.name = name;
+  }
+};
+
+const groups = {};
+const getGroup = (name) => {
+  if (!name) name = "default"
+  const g = groups[name];
+  if (g) return g;
+  const g2 = groups[name] = new Group(name);
+  return g2;
+};
 
 export const handleWebSocket = (socket) => {
   socket.onopen = () => {
@@ -8,27 +23,29 @@ export const handleWebSocket = (socket) => {
   };
   socket.onmessage = (event) => {
     const data = event.data;
+    const g = getGroup(data.group);
+    if (!g) return;
     const t = data.type;
     //console.log(data, t);
-    console.log(t);
+    console.log(g.name, t);
     if (t == "init_cam") {
-      if (cam) {
+      if (g.cam) {
         try {
-          cam.send({ type: "end_cam" });
+          g.cam.send({ type: "end_cam" });
         } catch (e) {
         }
       }
-      cam = socket;
+      g.cam = socket;
     } else if (t == "init_client") {
-      if (cam && !running) {
-        cam.send({ type: "start_cam" });
+      if (g.cam && !g.running) {
+        g.cam.send({ type: "start_cam" });
         console.log("start cam");
-        running = true;
+        g.running = true;
       }
-      clients.push(socket);
+      g.clients.push(socket);
     } else if (t == "cam_image") {
-      console.log("img transfer", clients.length);
-      clients.forEach(i => {
+      console.log("img transfer", g.clients.length);
+      g.clients.forEach(i => {
         try {
           i.send({ type: "cam_image", img: data.img });
         } catch (e) {
@@ -37,17 +54,20 @@ export const handleWebSocket = (socket) => {
     }
   };
   socket.onclose = () => {
-    for (let i = 0; i < clients.length; i++) {
-      if (clients[i] == socket) {
-        clients.splice(i, 1);
-        break;
-      }
-    }
-    if (clients.length == 0) {
-      if (running) {
-        running = false;
-        cam.send({ type: "stop_cam" });
-        console.log("stop cam");
+    A: for (const name in groups) {
+      const g = groups[name];
+      for (let i = 0; i < g.clients.length; i++) {
+        if (g.clients[i] == socket) {
+          g.clients.splice(i, 1);
+          if (g.clients.length == 0) {
+            if (g.running) {
+              g.running = false;
+              g.cam.send({ type: "stop_cam" });
+              console.log("stop cam");
+            }
+          }
+          break A;
+        }
       }
     }
     console.log("DISCONNECTED");
